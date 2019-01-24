328106b7172e8546c877400b2712b936f19cdf7a
Eddy Caldas <eddycaldas@live.com>
2019-01-24 12:23:53 -0700

    hard code image

---
+++ App.js
@@ -0,0 +1,57 @@
+
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
+import placeImage from './src/assets/beautiful-place.jpg';
+
+export default class App extends Component {
+
+    state = {
+      places: []
+    };
+
+    placeAddedHandler = (placeName) => {
+      this.setState(prevState => {
+        return {
+          places: prevState.places.concat({
+            key: Math.random(), 
+            name: placeName,
+            image: placeImage
+          })
+        }
+      })
+    }
+
+    placeDeleteHandler = key => {
+      this.setState(prevState => {
+        return {
+          places: prevState.places.filter(place => {
+            return place.key !== key;
+        })
+        }
+      })
+    }
+
+  render() {
+
+    return (
+      <View style={styles.container}>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
+        <PlaceList places={this.state.places}
+          onItemDeleted={this.placeDeleteHandler}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
---
+++ src/components/PlaceList/PlaceList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+import ListItem from '../ListItem/ListItem'
+
+const placeList = props => {    
+    
+    return (
+        <FlatList 
+            style={styles.output}
+            data={props.places}
+            renderItem={(info) => (
+                <ListItem 
+               placeName={info.item.name}
+               placeImage={info.item.image} 
+               onItemPressed={() => props.onItemDeleted(info.item.key)}
+        /> 
+            )}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    output: {
+      width: '100%'
+    }  
+})
+
+export default placeList;
