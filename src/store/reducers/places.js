595b66245b98dba2c5d821fb0db83b33940c154e
Eddy Caldas <eddycaldas@live.com>
2019-01-30 22:29:38 -0700

    setting up reducer after actions

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,10 @@
+import {
+    ADD_PLACE,
+    DELETE_PLACE,
+    DESELECT_PLACE,
+    SELECT_PLACE
+} from '../actions/actionTypes';
+
 const initialState = {
     places: [],
     selectedPlace: null
@@ -5,6 +12,42 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+
+        case ADD_PLACE: 
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: Math.random(), 
+                    name: action.placeName, //from places.js
+                    image: {
+                      uri: 'http://eddycaldaswebsite.surge.sh/images/pic00.jpg'
+                    }
+                  })
+            }
+
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            };
+
+        case DESELECT_PLACE: 
+            return {
+                ...state,
+                selectedPlace: null
+            }
+
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => {
+                    return place.key === action.placeKey; //from places.js
+                  })
+            }
+
         default: 
         return state;
     }
