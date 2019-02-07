d67bcef60dff68f2c9db7a61c8fd42e4d282b1ea
Eddy Caldas <eddycaldas@live.com>
2019-02-07 14:42:35 -0700

    connecting screens to redux

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { Navigation } from 'react-native-navigation';
 
+import { Provider } from 'react-redux';
+
 import AuthScreen from './src/Screens/Auth/Auth';
 import SharePlaceScreen from './src/Screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/Screens/FindPlace/FindPlace';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
 
 
 //register screens
-Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
-Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen);
+Navigation.registerComponent("awesome-places.AuthScreen", 
+    () => AuthScreen, store, Provider);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", 
+    () => SharePlaceScreen, store, Provider);
+Navigation.registerComponent("awesome-places.FindPlaceScreen", 
+    () => FindPlaceScreen, store, Provider);
 
 // starting the screen page -app
 Navigation.startSingleScreenApp({
